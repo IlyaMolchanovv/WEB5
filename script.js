419d27cb30657e3689d5dd357e2503b38787b0b0
IlyaMolchanovv <90561936+IlyaMolchanovv@users.noreply.github.com>
2021-10-22 10:58:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function calculate() {
     price = document.getElementById('product_price').value; 
     count = document.getElementById('product_count').value; 
     if (price >= 0 && count >= 0) {sum = parseInt(price) * parseInt(count);}
-    else sum = NaN
+    else sum = NaN;
     document.getElementById('sum').innerHTML="Итоговая стоимость: "+ sum + "руб."; 
     return false;
 }
